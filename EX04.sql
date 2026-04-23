1ec93943bd2881e331a40c75184a640977f26cd9
qu04nh-dev <quocanh.04.007@gmail.com>
2026-04-24 00:07:49 +0700

    IT202_SS02_EX04

--- EX04.sql
+++ EX04.sql
@@ -12,3 +12,6 @@ SET Phone = LPAD(Phone, 10, '0')
 WHERE UserID > 0;
 
 SELECT * FROM USERS;
+-- BIGINT: Tiết kiệm dung lượng lưu trữ hơn một chút. Mất số 0 ở đầu, không lưu được mã quốc tế dạng chuỗi, tính linh hoạt thấp
+-- VARCHAR: Lưu được số 0 ở đầu, tốn thêm vài byte dũ liệu cho mỗi bản ghi, tính linh hoạt cao
+-- Quyết định: Chọn VARCHAR. Mặc dù tốn thêm một ít dung lượng, nhưng đây là lựa chọn duy nhất đảm bảo tính toàn vẹn dữ liệu
